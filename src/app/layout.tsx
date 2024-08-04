44c4e14dc19da7a0672de2bbaa8848287f3292ff
zakaria hadraoui <zikoxo05@gmail.com>
2024-08-04 19:30:21 +0100

    hipapana

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+      <meta name="google-site-verification" content="Ol1_JKT5UJTk5gEDKeldzGmTOQYjjhZBmHSZkQ30ULw" />
+      </head>
       <body className={'dark:bg-[#020817] max-w-7xl mx-auto'}>
         <Providers>
         <Navbar/>
